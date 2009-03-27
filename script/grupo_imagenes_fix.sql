6e9c207dff4867b2e37d3b16923a9a08d8909863
unknown <Carlos@.(none)>
2009-03-27 21:52:37 +0100

    added script

---
+++ script/grupo_imagenes_fix.sql
@@ -0,0 +1,6 @@
+ALTER TABLE imagen MODIFY COLUMN id_grupo_imagen INTEGER UNSIGNED DEFAULT NULL;
+ALTER TABLE imagen ADD INDEX id_grupo_imagenes(id_grupo_imagen),
+ ADD CONSTRAINT imagen_ibfk_5 FOREIGN KEY imagen_ibfk_5 (id_grupo_imagen)
+    REFERENCES grupo_imagenes (id_grupo_imagenes)
+    ON DELETE SET NULL
+    ON UPDATE NO ACTION;
