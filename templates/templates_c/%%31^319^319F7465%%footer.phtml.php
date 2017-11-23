e6fc2801859d08de945c6d998637cccfdd21b1b4
Carlos Alonso <carlos.alonso@cabify.com>
2017-11-23 21:36:22 +0100

    deleted compiled files

--- templates/templates_c/%%31^319^319F7465%%footer.phtml.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2009-06-18 17:22:13
-         compiled from C:/dev/apache/htsdocs/globaldent/templates/templates/web/common/footer.phtml */ ?>
-<div class="bottom">
-		<img src="<?php echo $this->_tpl_vars['img_path_base']; ?>
-logo_pie.jpg" alt="Logo GlobalDent" align="middle" height="50" width="60"/>
-		<p>Copyright &copy; GlobalDent 08</p>
-	</div>
-</body>
-</html>
--- templates/templates_c/%%82^824^82447A1F%%footer.phtml.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2009-06-18 17:22:50
-         compiled from C:/dev/apache/htsdocs/globaldent/templates/templates/web/gbdapp/common/footer.phtml */ ?>
-<div class="pie">
-		<img src="<?php echo $this->_tpl_vars['img_path_base']; ?>
-logo_pie.jpg" alt="Logo GlobalDent" align="middle" height="50" width="60"/>
-		<p>Copyright &copy; GlobalDent 08</p>
-	</div>
-</body>
-</html>
